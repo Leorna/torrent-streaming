2f3708d96ae886eae226cc0913a13fb32162e6de
Leorna <42484610+Leorna@users.noreply.github.com>
2019-04-06 23:43:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+const input = require('readline-sync')
+
 (function(){
 	async function iniciar (){
 		const torrent = require('torrent-search-api')
 		torrent.enableProvider('Torrent9')
-		let pesquisa = 'Filme'
+		let pesquisa = input.question('Digite o que deseja pesquisar: ')
 		const torrents = await torrent.search(pesquisa, 'Movies', 20)
 		let mgLink = torrent.getMagnet(torrents[0])
 	return mgLink
